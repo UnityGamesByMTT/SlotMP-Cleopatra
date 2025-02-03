17139e8d04922193add89961f85016ae1f331887
Mehul Parakh <mehul@underpinservices.com>
2025-02-03 14:16:27 +0530

    CHORE: Low Balacne Fix

--- Assets/Scripts/Functionality/SlotBehaviour.cs
+++ Assets/Scripts/Functionality/SlotBehaviour.cs
@@ -348,7 +348,7 @@ public class SlotBehaviour : MonoBehaviour
         if (LineBet_text) LineBet_text.text = SocketManager.initialData.Bets[BetCounter].ToString();
         if (TotalBet_text) TotalBet_text.text = (SocketManager.initialData.Bets[BetCounter] * Lines).ToString();
         currentTotalBet = SocketManager.initialData.Bets[BetCounter] * Lines;
-        CompareBalance();
+        // CompareBalance();
     }
 
     #region InitialFunctions
